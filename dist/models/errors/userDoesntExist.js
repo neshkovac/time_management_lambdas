15c8e58c4bdbafc8b644318daca9eb29a1f7102e
Lazar Neskovic <l.neskovic@levi9.com>
2021-12-24 14:11:05 +0100

    Added all the handlers

---
+++ dist/models/errors/userDoesntExist.js
@@ -0,0 +1,79 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/models/errors/customError.ts":
+/*!******************************************!*\
+  !*** ./src/models/errors/customError.ts ***!
+  \******************************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CustomError = void 0;
+class CustomError extends Error {
+    constructor(message) {
+        super(message);
+    }
+}
+exports.CustomError = CustomError;
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+/*!**********************************************!*\
+  !*** ./src/models/errors/userDoesntExist.ts ***!
+  \**********************************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.UserDoesntExistError = void 0;
+const customError_1 = __webpack_require__(/*! ./customError */ "./src/models/errors/customError.ts");
+class UserDoesntExistError extends customError_1.CustomError {
+    constructor(message) {
+        super(message);
+        this.statusCode = 400;
+    }
+    serializeErrors() {
+        return [{ statusCode: this.statusCode, message: this.message }];
+    }
+}
+exports.UserDoesntExistError = UserDoesntExistError;
+
+})();
+
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;

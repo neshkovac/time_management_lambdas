784765772cf3aa4ff7d0aca8ef79cd23614aac69
Lazar Neskovic <l.neskovic@levi9.com>
2021-12-29 14:01:23 +0100

    refactor: Added constants file for user and schedule info

---
+++ dist/util/constants.js
@@ -0,0 +1,28 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+/*!*******************************!*\
+  !*** ./src/util/constants.ts ***!
+  \*******************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.SCHEDULE_TABLE_NAME = exports.USER_TABLE_NAME = exports.SCHEDULE_REGION = exports.USER_REGION = void 0;
+const USER_REGION = 'us-east-1';
+exports.USER_REGION = USER_REGION;
+const SCHEDULE_REGION = 'us-east-1';
+exports.SCHEDULE_REGION = SCHEDULE_REGION;
+const USER_TABLE_NAME = 'user-table';
+exports.USER_TABLE_NAME = USER_TABLE_NAME;
+const SCHEDULE_TABLE_NAME = 'schedule-table';
+exports.SCHEDULE_TABLE_NAME = SCHEDULE_TABLE_NAME;
+
+})();
+
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;
